edda717a2466f4026afb758db1d2d79c9cd946bb
AhernEYeldarB <ahernebradley@gmail.com>
2021-02-20 23:31:09 +0000

    Modify blob storage to read and decode JSON files

--- BlobWriterCSharp.cs
+++ BlobWriterCSharp.cs
@@ -2,13 +2,14 @@ using System.Collections.Generic;
 using System.IO;
 using Microsoft.Azure.WebJobs;
 using System;
+using System.Text;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 public class BlobWriterCSharp
 {
     [FunctionName("BlobWriterCSharp")]
-    public static void Run([BlobTrigger("fyp1/{name}", Connection = "AzureWebJobsStorage")] Stream InStream, string name, ILogger log,
-        [Blob("fyp1/{name}")] Byte[] OutArray)
+    public static void Run([BlobTrigger("test1/{name}", Connection = "AzureWebJobsStorage")] Stream InStream, string name, ILogger log,
+        [Blob("test1/{name}", FileAccess.Write)] Stream test)
     {
         byte[] byteArray;
         log.LogInformation("1");
@@ -16,13 +17,8 @@ public class BlobWriterCSharp
         {
             log.LogInformation("2");
             byteArray = ReadAllBytes(InStream);
-            for (int i = 0; i < InStream.Length; i++)
-            {
-                int temp = byteArray[i];
-                byteArray[i] *= 2;
-                log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {byteArray.Length} Bytes, value: {byteArray[i]} = {temp} x 2");
-            }
-            OutArray = byteArray;
+            string utfString = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {byteArray.Length} Bytes, value: {byteArray} = {utfString}");
         }
         else
         {
@@ -30,9 +26,9 @@ public class BlobWriterCSharp
             int[] intArray = { 1, 2, 3, 4, 5 };
             byte[] result = new byte[intArray.Length * sizeof(int)];
             Buffer.BlockCopy(intArray, 0, result, 0, result.Length);
-            OutArray = result;
         }
         log.LogInformation("4");
+        return;
     }
 
     public static byte[] ReadAllBytes(Stream instream)
@@ -46,4 +42,58 @@ public class BlobWriterCSharp
             return memoryStream.ToArray();
         }
     }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    // Used as an ORM layer 
+    public class HumanIdentity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Root
+    {
+        public string _id { get; set; }
+        public int index { get; set; }
+        public string guid { get; set; }
+        public bool isActive { get; set; }
+        public string balance { get; set; }
+        public string picture { get; set; }
+        public int age { get; set; }
+        public string eyeColor { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string about { get; set; }
+        public DateTime registered { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public List<string> tags { get; set; }
+        public List<HumanIdentity> HumanIdentities { get; set; }
+        public string greeting { get; set; }
+        public string favoriteFruit { get; set; }
+    }
 }
+// {
+//   "generatedBy": "Microsoft.NET.Sdk.Functions-3.0.7",
+//   "configurationSource": "attributes",
+//   "bindings": [
+//     {
+//       "type": "orchestrationTrigger",
+//       "name": "context"
+//     },
+//     {
+//       "dataType": "stream",
+//       "direction": "in",
+//       "connection": "AzureWebJobsStorage",
+//       "path": "test1/random-personal-info1",
+//       "name": "InStream"
+//     }
+//   ],
+//   "disabled": false,
+//   "scriptFile": "../bin/csFyp.dll",
+//   "entryPoint": "Company.Function.readFromBlob.RunOrchestrator"
+// }
