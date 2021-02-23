2fc141c15030720e67837044fb2f5e2c6e75e728
AhernEYeldarB <ahernebradley@gmail.com>
2021-02-23 11:29:12 +0000

    Add script for emulator start

---
+++ start-storage-emulator.cmd
@@ -0,0 +1,2 @@
+@REM This is used for the older emulator which seems to only work for streaming data from blob storage properly
+"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe" start
