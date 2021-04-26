8f8e3e766253c144edd54fc4c861de158449e680
AhernEYeldarB <ahernebradley@gmail.com>
2021-04-26 21:43:01 +0100

    pruned using staements

--- readFromBlob.cs
+++ readFromBlob.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using Microsoft.Azure.WebJobs;
